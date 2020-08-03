cf4268009ddcceb07ac37f1d51051ea071dcab8c
zayzay500 <lindsay.pierce@yale.edu>
2020-08-03 10:08:09 -0400

    done

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -4,8 +4,23 @@ class Api::V1::NotesController < ApplicationController
     render json: notes
   end
   
+  def create
+    note = Note.create(notes_params)
+    render json: note
+  end
+  
+  def update
+    note = Note.find(params[:id])
+    note.update(notes_params)
+    render json: note
+  end
+  
   def show
     note = Note.find(params[:id])
     render json: note
+  end
+  
+  def notes_params
+    params.require(:note).permit(:text, :proposal_id)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,10 @@ Note.reset_pk_sequence
 Proposal.destroy_all
 Proposal.reset_pk_sequence
 
-Proposal.create(title: 'Miracle Jelly', author: 'Lindsay Pierce', description: 'Not sure about this yet', link: 'https://docs.google.com/document/d/1gdV7NauC3sH7esyy_2sqgt9c6luDyUHPHkaNr74IEDw/edit')
+Proposal.create(title: 'Miracle Jelly', author: 'Lindsay Pierce', description: 'Memoir focused on going through college in the time of corona, beginning with the shutdown of New York and ending whenever this is actually over', link: 'https://docs.google.com/document/d/1gdV7NauC3sH7esyy_2sqgt9c6luDyUHPHkaNr74IEDw/edit')
 Proposal.create(title: 'Home Alone: Quarantine Edition', author: 'Baby Kevin', description: 'A take on living alone in the coronavirus and the search for human connection', link: 'https://docs.google.com/document/d/1u5oCzFPCfxH0nmGgez5Kukcv9gaTyj4lRulbT2p_vnY/edit')
+Proposal.create(title: 'Test Prep 101: How To Ace Your SATs', author: 'Brian Adams', description: 'A book guiding students through SAT test prep, including how to deal with overbearing parents and which courses are actually worth your time.')
 
-Note.create(proposal: Proposal.first, text: "still working on perspective")
-Note.create(proposal: Proposal.first, text: "Make an effort to narrow your scope here")
-Note.create(proposal: Proposal.first, text: "Everyone loves humor, try to find some levity here and there")
+Note.create(proposal: Proposal.first, text: "Still working on the perspective in the first few chapters, need a strategic approach")
+Note.create(proposal: Proposal.first, text: "Need to narrow scope in the latter half")
+Note.create(proposal: Proposal.first, text: "Everyone loves humor, try to find some levity in stuff like the bathroom and train scenes in the third chapter")
